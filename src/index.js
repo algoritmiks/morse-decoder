8b6b612a094831905a5eb968ddaf83d5c8772ccc
Aleksey Osipov <alexey11rus@gmail.com>
2020-03-03 13:50:55 +0300

    feat: morse done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/(.{1,10})/gim) || '';
+    let result = '';
+    arr.forEach(el => {
+        let current = '';
+        if (el == "**********") {
+            result += ' ';
+        } else {
+            current = el.replace(/10/g, '.');
+            current = current.replace(/11/g, '-');
+            current = current.replace(/00/g, '');
+            result += MORSE_TABLE[current];
+        }
+    });
+    return result;
 }
 
 module.exports = {
